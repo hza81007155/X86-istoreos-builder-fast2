9f0836057b7f5e9b895d48902440a0a636023e36
hza81007155 <800755@qq.com>
2026-08-25 23:52:04 +0800

    Update 99-custom.sh

--- files/uci-defaults/99-custom.sh
+++ files/uci-defaults/99-custom.sh
@@ -83,7 +83,7 @@ uci commit
 
 # 设置作者描述信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="iStoreOS VERXXXX"
+NEW_DESCRIPTION="iStoreOS by hza800755"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
